4766af6f2f796a325eca666bb06d366c2a7cb517
AmielRe <amieli1999@gmail.com>
2022-11-19 10:43:04 +0200

    Create shopping list.

--- mission1/src/index.js
+++ mission1/src/index.js
@@ -6,9 +6,41 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h>Hey</h>
+        <ShoppingList></ShoppingList>
       </div>
     );
+  }
+}
+
+class Item extends React.Component {
+  render() {
+    return (
+      <li className="ListItem">
+        <div>
+          {this.props.name}
+        </div>
+        <div>
+          {this.props.price} nis
+        </div>
+        <img src={`/images/${this.props.image}`}></img>
+      </li>
+    );
+  }
+}
+
+class ShoppingList extends Component {
+  renderItem(name, price, image){
+    return <Item name={name} price={price} image={image}></Item>;
+  }
+  
+  render() {
+    return(
+      <ul className="shoppingList">
+        {this.renderItem("Milk", "10", "milk.png")}
+        {this.renderItem("Bread", "8", "bread.png")}
+        {this.renderItem("Eggs", "12", "eggs.png")}
+      </ul>
+    )
   }
 }
 
